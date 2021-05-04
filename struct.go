4d0f3d69afea15e67fbfd856ab0ff45b923b3918
Titanio Yudista <titanioyudista98@gmail.com>
2021-05-04 18:03:27 +0700

    update struct

--- struct.go
+++ struct.go
@@ -8,25 +8,36 @@ type Customer struct {
 	// Married bool
 }
 
+func (customer Customer) sayHai(name string) {
+	fmt.Println("Hallo", name, "My name is", customer.Name)
+}
+
+func (a Customer) sayHuu() {
+	fmt.Println("Huu from ", a.Name)
+}
+
 func main() {
 	// ! tiga cara memasukkan atau input value di Struct
 	var eko Customer
 	eko.Name = "eko"
 	eko.Address = "indonesia"
 	eko.Age = 30
-	fmt.Println(eko.Name)
-	fmt.Println(eko.Address)
-	fmt.Println(eko.Age)
-	fmt.Println(eko)
 
-	joko := Customer{
-		Name:    "joko",
-		Address: "Cirebon",
-		Age:     35,
-	}
-	fmt.Println(joko)
+	eko.sayHai("Joko")
+	eko.sayHuu()
+	// fmt.Println(eko.Name)
+	// fmt.Println(eko.Address)
+	// fmt.Println(eko.Age)
+	// fmt.Println(eko)
 
-	budi := Customer{"Budi", "Jakrata", 45}
-	fmt.Println(budi)
+	// joko := Customer{
+	// 	Name:    "joko",
+	// 	Address: "Cirebon",
+	// 	Age:     35,
+	// }
+	// fmt.Println(joko)
+
+	// budi := Customer{"Budi", "Jakrata", 45}
+	// fmt.Println(budi)
 
 }
